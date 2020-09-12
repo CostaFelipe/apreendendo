74ffe678b6fa109fab78c4aaaa84e0de6bc5cd79
costadefelipe@gmail.com <costadefelipe@gmail.com>
2020-09-12 18:44:42 -0300

    laços for: soma e quant

---
+++ python-apreender/04_iteracao/04_lacos_for_iteracao.py
@@ -0,0 +1,9 @@
+amigos = ['Jose', 'Gleice', 'Sara']
+for amigo in amigos:
+    print('Feliz Ano Novo,',amigo)
+print('Feito!')
+
+nomes = ['Sarah', 'Esther', 'Linda Gomes', 'Mariana']
+for nome in nomes:
+    print(nome)
+print("Feito!")
---
+++ python-apreender/04_iteracao/05_padroes_laco_iteracao.py
@@ -0,0 +1,12 @@
+contador = 0
+array = [1.00, 1.00, 1.00, 1.00, 1.00, 1.00]
+for itervar in array:
+    contador = contador + 1
+
+print('contagem:', contador)
+
+total = 0
+for itervar in array:
+    total = total + itervar
+
+print('Total:', total)
