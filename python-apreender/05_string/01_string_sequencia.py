7d71ab6937f7855dba505a0e525007ee3b340a6f
costadefelipe@gmail.com <costadefelipe@gmail.com>
2020-09-15 13:23:51 -0300

    init string

---
+++ python-apreender/05_string/01_string_sequencia.py
@@ -0,0 +1,4 @@
+fruta = 'banana'
+letra = fruta[1]
+letra = fruta[0]
+print(letra)
---
+++ python-apreender/05_string/02_comprimento_string.py
@@ -0,0 +1,13 @@
+fruta = 'banana'
+print(len(fruta))
+
+comprimento = len(fruta)
+ultima = fruta[comprimento - 1]
+
+print(ultima)
+
+indice = 0
+while indice < len(fruta):
+    letra = fruta[indice]
+    print(letra)
+    indice = indice + 1
